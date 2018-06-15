2b107a09039612343416692090d9335b93077cfe
mj <maj1337+git@gmail.com>
2018-06-15 13:53:08 +0200

    such rafactor, much nice

--- example.pro.py
+++ example.pro.py
@@ -1,13 +1,17 @@
 import rtmidi
 
-mo = rtmidi.MidiOut()
-for port_no in range(mo.get_port_count()):
-        port_name = mo.get_port_name(port_no)
-        print("MIDI out:", port_name)
-        if port_name.find('Launchpad Pro Standalone Port') > -1: 
-            # or 'Launchpad Pro Standalone Port'
-            midi_port = mo.open_port(port_no)
-            
-position = 22
-color = 1
-midi_port.send_message([240, 0, 32, 41, 2, 16, 10, position, color, 247])
+def main():
+    mo = rtmidi.MidiOut()
+    for port_no in range(mo.get_port_count()):
+            port_name = mo.get_port_name(port_no)
+            print("MIDI out:", port_name)
+            if port_name.find('Launchpad Pro Standalone Port') > -1: 
+                # or 'Launchpad Pro Standalone Port'
+                midi_port = mo.open_port(port_no)
+                
+    position = 22
+    color = 1
+    midi_port.send_message([240, 0, 32, 41, 2, 16, 10, position, color, 247])
+
+if (__name__ == '__main__'):
+    main();
--- example.py
+++ example.py
@@ -1,13 +1,19 @@
 import rtmidi
 
-mo = rtmidi.MidiOut()
-for port_no in range(mo.get_port_count()):
-        port_name = mo.get_port_name(port_no)
-        print("MIDI out:", port_name)
-        if port_name.find('Launchpad Mini') > -1: 
-            # or 'Launchpad Pro Standalone Port'
-            midi_port = mo.open_port(port_no)
-            
-color = 28
-position = 7
-midi_port.send_message([0x90, 7, 28])
+def main():
+    mo = rtmidi.MidiOut()
+    for port_no in range(mo.get_port_count()):
+            port_name = mo.get_port_name(port_no)
+            print("MIDI out:", port_name)
+            if port_name.find('Launchpad Mini') > -1: 
+                # or 'Launchpad Pro Standalone Port'
+                midi_port = mo.open_port(port_no)
+                
+    color = 28
+    position = 7
+    midi_port.send_message([0x90, 7, 28])
+
+if (__name__ == '__main__'):
+    main();
+else:
+    print("wat?", __name__)
