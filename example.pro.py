4c658f016c43336ad407e3ff9b516a2c820539a4
mj <maj1337+git@gmail.com>
2018-06-15 13:47:52 +0200

    cleanup and pro

--- example.pro.py
+++ example.pro.py
@@ -4,10 +4,10 @@ mo = rtmidi.MidiOut()
 for port_no in range(mo.get_port_count()):
         port_name = mo.get_port_name(port_no)
         print("MIDI out:", port_name)
-        if port_name.find('Launchpad Mini') > -1: 
+        if port_name.find('Launchpad Pro Standalone Port') > -1: 
             # or 'Launchpad Pro Standalone Port'
             midi_port = mo.open_port(port_no)
             
-color = 28
-position = 7
-midi_port.send_message([0x90, 7, 28])
+position = 22
+color = 1
+midi_port.send_message([240, 0, 32, 41, 2, 16, 10, position, color, 247])
